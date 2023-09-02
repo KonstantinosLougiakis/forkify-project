495006df027fdbc506be2089535808ce6389de64
KonstantinosLougiakis <lougiokostas@gmail.com>
2023-09-02 14:39:18 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1af1337b-7196-4dd7-b574-13aead075ea7';
+export const MODAL_CLOSE_SEC = 2.5;
+
